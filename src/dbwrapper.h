1fb7017c43561ffd741bf0b9dfd40158518086d9
toromame8373 <toshnobu0@gmail.com>
2026-02-24 22:07:50 +0900

    Update dbwrapper.h

--- src/dbwrapper.h
+++ src/dbwrapper.h
@@ -1,9 +1,9 @@
-// Copyright (c) 2012-present The Bitcoin Core developers
+// Copyright (c) 2012-present The tocoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_DBWRAPPER_H
-#define BITCOIN_DBWRAPPER_H
+#ifndef tocoin_DBWRAPPER_H
+#define tocoin_DBWRAPPER_H
 
 #include <attributes.h>
 #include <serialize.h>
@@ -272,4 +272,4 @@ public:
     }
 };
 
-#endif // BITCOIN_DBWRAPPER_H
+#endif // tocoin_DBWRAPPER_H
