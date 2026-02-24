1b4ba9685deac11f1f7275854c6d12f83f200128
toromame8373 <toshnobu0@gmail.com>
2026-02-24 21:58:07 +0900

    Update clientversion.h

--- src/clientversion.h
+++ src/clientversion.h
@@ -1,17 +1,17 @@
-// Copyright (c) 2009-present The Bitcoin Core developers
+// Copyright (c) 2009-present The tocoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_CLIENTVERSION_H
-#define BITCOIN_CLIENTVERSION_H
+#ifndef tocoin_CLIENTVERSION_H
+#define tocoin_CLIENTVERSION_H
 
 #include <util/macros.h>
 
-#include <bitcoin-build-config.h> // IWYU pragma: keep
+#include <tocoin-build-config.h> // IWYU pragma: keep
 
 // Check that required client information is defined
 #if !defined(CLIENT_VERSION_MAJOR) || !defined(CLIENT_VERSION_MINOR) || !defined(CLIENT_VERSION_BUILD) || !defined(CLIENT_VERSION_IS_RELEASE) || !defined(COPYRIGHT_YEAR)
-#error Client version information missing: version is not defined by bitcoin-build-config.h or in any other way
+#error Client version information missing: version is not defined by tocoin-build-config.h or in any other way
 #endif
 
 //! Copyright string used in Windows .rc files
@@ -41,4 +41,4 @@ std::string LicenseInfo();
 
 #endif // RC_INVOKED
 
-#endif // BITCOIN_CLIENTVERSION_H
+#endif // tocoin_CLIENTVERSION_H
