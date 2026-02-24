2b20cfefe7377d53de28cba7db5a861949bf62da
toromame8373 <toshnobu0@gmail.com>
2026-02-24 22:10:42 +0900

    Update deploymentinfo.h

--- src/deploymentinfo.h
+++ src/deploymentinfo.h
@@ -1,9 +1,9 @@
-// Copyright (c) 2016-present The Bitcoin Core developers
+// Copyright (c) 2016-present The tocoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_DEPLOYMENTINFO_H
-#define BITCOIN_DEPLOYMENTINFO_H
+#ifndef tocoin_DEPLOYMENTINFO_H
+#define tocoin_DEPLOYMENTINFO_H
 
 #include <consensus/params.h>
 
@@ -32,4 +32,4 @@ inline std::string DeploymentName(Consensus::DeploymentPos pos)
 
 std::optional<Consensus::BuriedDeployment> GetBuriedDeployment(std::string_view deployment_name);
 
-#endif // BITCOIN_DEPLOYMENTINFO_H
+#endif // tocoin_DEPLOYMENTINFO_H
